95ab58b781323f4034d223348cd98a3692845ac1
Omega579 <88238063+iDevYT@users.noreply.github.com>
2022-02-07 14:15:55 +0100

    start

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace better_comments
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {   
+            //FREESHELL v1.0
+            Console.WriteLine("Welcome to Freeshell!");
+            string? input = Console.ReadLine();
+
+
+        }
+    }
+}
