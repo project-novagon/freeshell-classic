4474b5685af7800b485a75625922af11a46ce1c4
Omega579 <88238063+iDevYT@users.noreply.github.com>
2022-03-05 20:07:47 +0100

    IT FINALLY WORKS 🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳

--- src/Program.cs
+++ src/Program.cs
@@ -15,33 +15,30 @@ namespace freeshell
 
         public static void Main(string[] args)
         {   
+            bool commands = true;
+            while (commands) 
+            {
             Console.ForegroundColor = Color.Green;
             string welcome = FiggleFonts.Standard.Render("Welcome To Freeshell!");
-            Console.Write(">");
             System.Console.WriteLine(welcome);
+            Console.Write(">");
             string? input = Console.ReadLine();
             Console.WriteLine($"running {input}");
-            bool commands = true;
-            while (commands == true) 
-            {
             switch (input)
                 {
                  case "help":
                     System.Console.WriteLine("FRESHELL.NET help \n no help now");
-                commands = false;
                     break;
                 case "exit":
                     Console.ForegroundColor = Color.White;
                     Environment.Exit(0);
-                    commands = false; 
+                    commands = false;
                     break;
                 case "proj":
                 System.Console.WriteLine("FRESHELL.NET1 by iDev \n GH repo: https://github.com/iDevYT/freeshell \n Enjoy! ");
-                commands = false;
                 break;
                 default:
             	System.Console.WriteLine($"{input} Not Found. Type help for help.");
-                commands = false;
                 break;	
             }   
             }           
