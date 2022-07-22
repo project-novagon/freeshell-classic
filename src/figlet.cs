57d9748270ae1efe9d74ab891325835dbe208f2c
iDevYT <alphangred57@gmail.com>
2022-07-22 19:55:57 +0200

    QOL 1

--- src/figlet.cs
+++ src/figlet.cs
@@ -7,10 +7,19 @@ namespace freeshell
     {
         public static void Run(string? input)
         {
+            bool commands = true;
+            while(commands)
+            {
             #nullable enable
             input = Console.ReadLine();
             string figletinput = FiggleFonts.Standard.Render(input);
             System.Console.WriteLine(figletinput);
+                if (input == null)
+                {
+                    System.Console.WriteLine("Please enter a string to be converted to figlet");
+                    commands = false;           
+                }
+            } 
         }
     } 
 }
