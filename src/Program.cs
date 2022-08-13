e6a32e919b5418becec9abae0bfe2eabeba75ea2
iDevYT <alphangred57@gmail.com>
2022-08-13 21:24:16 +0200

    added colors

--- src/Program.cs
+++ src/Program.cs
@@ -49,7 +49,46 @@ namespace freeshell
                         Console.WriteLine(welcome);
                         break;
                     case "color":
-                        Console.WriteLine("color: ");
+                        Console.WriteLine("color: color is a freeshell utility that changes the system color. \n example: color --set {color}  \n colors: red, green, yellow, blue, magenta, cyan, white, gray, black");
+                        break;
+                    case "color --set":
+                        Console.WriteLine("color --set: color is a freeshell utility that changes the system color. \n example: color --set red  \n colors: red, green, yellow, blue, magenta, cyan, white, gray, black");
+                        break;
+                    case "color --set red":
+                        Console.ForegroundColor = Color.Red;
+                        Console.WriteLine("red");
+                        break;
+                    case "color --set green":
+                        Console.ForegroundColor = Color.Green;
+                        Console.WriteLine("green");
+                        break;
+                    case "color --set yellow":
+                        Console.ForegroundColor = Color.Yellow;
+                        Console.WriteLine("yellow");
+                        break;
+                    case "color --set blue":
+                        Console.ForegroundColor = Color.Blue;
+                        Console.WriteLine("blue");
+                        break;
+                    case "color --set magenta":
+                        Console.ForegroundColor = Color.Magenta;
+                        Console.WriteLine("magenta");
+                        break;
+                    case "color --set cyan":
+                        Console.ForegroundColor = Color.Cyan;
+                        Console.WriteLine("cyan");
+                        break;
+                    case "color --set white":
+                        Console.ForegroundColor = Color.White;
+                        Console.WriteLine("white");
+                        break;
+                    case "color --set gray":
+                        Console.ForegroundColor = Color.Gray;
+                        Console.WriteLine("gray");
+                        break;
+                    case "color --set black":
+                        Console.ForegroundColor = Color.Black;
+                        Console.WriteLine("black");
                         break;
                     default:
                         Console.WriteLine($"{input} Not Found. Type help for help.");
