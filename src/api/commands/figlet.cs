0feb05d2d906c9d25ebc6d0a302533190dbc8671
iDevYT <alphangred57@gmail.com>
2023-02-06 20:02:47 +0100

    added exit

--- src/api/commands/figlet.cs
+++ src/api/commands/figlet.cs
@@ -9,15 +9,16 @@ namespace freeshell
         {
             //figlet 1.0.0 by iDevYT
             bool commands = true;
+            System.Console.WriteLine("Welcome To Figlet! type \"exit\" to exit");
             while(commands)
             {
             #nullable enable
             input = Console.ReadLine();
             string figletinput = FiggleFonts.Standard.Render(input);
             System.Console.WriteLine(figletinput);
-                if (input == null)
+                if (input == "exit")
                 {
-                    System.Console.WriteLine("Please enter a string to be converted to figlet");
+                    System.Console.WriteLine("Done!");
                     commands = false;           
                 }
             } 
