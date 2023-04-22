866f23423c58e94b023f4d3b5923ece019903e63
iDevYT <alphangred57@gmail.com>
2023-04-22 12:49:52 +0200

    idk

--- src/api/fs.cs
+++ src/api/fs.cs
@@ -15,9 +15,8 @@ namespace freeshell
             var releases = client.Repository.Release.GetLatest("dvnlx", "freeshell");
             var latest = releases.Result;
             Console.WriteLine(
-                "The latest release is tagged at {0} and is named {1}",
-                latest.TagName,
-                latest.Name);
+                "The latest Freeshell release is{0}",
+                latest.TagName);
 
             Console.Write("\x1b[1mNOTE:\x1b[0m");
             System.Console.WriteLine(" If you dont have the newest version get it here:");
